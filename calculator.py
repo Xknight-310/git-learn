1aec062919797fc9161fa54d30f5671c86ecaad1
Xknight <216165154+Xkinght@users.noreply.github.com>
2025-06-13 16:21:17 +0200

    added extra check input of user is int

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ def add_num(x, y):
 def min_num(x, y):
     result = int(x) - int(y)
     return result
-x = input(print("give your x number... "))
-y = input(print("give your y number... "))
+x = int(input(print("give your x number... ")))
+y = int(input(print("give your y number... ")))
 print(f"the sum of {x} and {y} is {add_num(x, y)}")
 print(f"the substraction of {x} and {y} is {min_num(x, y)}")
